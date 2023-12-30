bff1a3e6a1a5d1f00d1ee3bfbf379762c66f7fb7
William <william.hagg1998@outlook.com>
2023-12-30 17:16:17 +0100

    Cheked my project for the last time before turning in.

--- Adressbook/Interfaces/IServiceResult.cs
+++ Adressbook/Interfaces/IServiceResult.cs
@@ -2,9 +2,19 @@
 
 namespace Adressbook.Interfaces
 {
+    /// <summary>
+    /// An interface representing the result of a service operation, containing a result object and a status.
+    /// </summary>
     public interface IServiceResult
     {
+        /// <summary>
+        /// Get's or set's the result of the object thats been operated.
+        /// </summary>
         object Result { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the service operation.
+        /// </summary>
         ServiceResultStatus Status { get; set; }
     }
 }
--- Adressbook/Services/FileService.cs
+++ Adressbook/Services/FileService.cs
@@ -4,19 +4,10 @@ namespace Adressbook.Services;
 
 public interface IFileService
 {
-    /// <summary>
-    /// Saves the content to a specified filepath.
-    /// </summary>
-    /// <param name="content">enter the content as a string</param>
-    /// /// <param name="filePath">enters the filepath of your choice</param>
-    /// <returns>If the filepath exists and is saved returns true, else fails. </returns>
+ 
     bool SaveContentToFile (string content);
 
-    /// <summary>
-    /// Gets content from a specific filepath
-    /// </summary>
-    /// <param name="filePath">enters the filepath of your choice</param>
-    /// <returns>returns as a string if the file is valid/exists, else if fails returns null.</returns>
+
     string GetContentFromFile();
 }
 
